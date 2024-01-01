d5bd819535b2fee766f3dbe14e7866d47368d2c9
Martin <martinryanmorris@gmail.com>
2024-01-01 16:58:22 +1000

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let manipulated = string.split("");
+  manipulated = manipulated.reverse();
+  let result = manipulated.join("");
+  return result;
 };
 
 // Do not edit below this line
