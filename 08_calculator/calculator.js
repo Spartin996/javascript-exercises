9e857cd38b88363e7f8d1d55ded720e52261dfc6
Martin <martinryanmorris@gmail.com>
2024-03-04 18:26:12 +1000

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
+const add = function(num1, num2) {
+  let answer = num1 + num2;
+  return answer;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  let answer = num1 - num2;
+  return answer;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+	let answer = array.reduce((sum, current) => sum + current, 0);
+  return answer;
+
+
+};
+
+const multiply = function(array) {
+  let answer = array.reduce((mult, current) => mult * current, 1);
+  return answer;
+
+};
+
+const power = function(num1, num2) {
+  let answer = num1 ** num2;
+  return answer;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1;
+  for (let index = 1; index <= num; index++) {
+    answer = answer * index;
+  }
+	return answer;
 };
 
 // Do not edit below this line
