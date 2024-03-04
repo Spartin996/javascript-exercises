10ff0bc71027612e045d4f9a597f3c665ed3210d
Martin <martinryanmorris@gmail.com>
2024-03-04 19:46:57 +1000

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //get rid of cases
+    let lowerStr = str.toLowerCase();
+    //remove all punction using regex
+    lowerStr = lowerStr.replace(/[^\w\s\']/g, "");
+    //remove whitespace
+    lowerStr = lowerStr.replace(/\s/g, "");
+
+
+    //reverse it to test
+    let reStr = "";
+    
+    reStr = lowerStr.split("");
+    reStr = reStr.reverse();
+    reStr = reStr.join("");
+
+
+    //check it
+    if (reStr === lowerStr) {
+        return true;
+    }
+
+    return false;
+
+
 
 };
 
