83c5887fc914835ac0bab6b2604328145a8e3d42
Martin <martinryanmorris@gmail.com>
2024-03-05 19:42:09 +1000

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(numSeq) {
+    //Goal is to return the Nth number in the fibonacci sequence
 
+    //Have a set of if's for error catching
+    if (numSeq < 0) {
+        return "OOPS";
+    }
+
+    //define some variables out of scope for the loop
+    let prevNum = 1;
+    let currentNum = 0;
+    let nextNum = 0;
+    
+    
+    // Lets make a fibonacci number
+    for (let index = 0; index < numSeq; index++) {
+        nextNum = prevNum + currentNum;
+        
+        //redifine the nums for the next loop
+        prevNum = currentNum;
+        currentNum = nextNum;
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
